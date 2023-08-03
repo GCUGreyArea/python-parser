0d08772622b3b0ed46fbafb428ff77deb103005e
Barry Robinson <barry.w.robinson64@gmail.com>
2023-08-03 07:39:39 +0100

    add python flask server for REST API

---
+++ server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from flask import Flask,request
+from parse import parse_message
+from framework import Framework
+
+
+app = Flask(__name__)
+f = Framework('rules')
+
+@app.route('/parse', methods=['POST'])
+def parse():
+   msg = request.form.get('message')
+
+   (p,t) = f.parse_fragment(msg,'root:regex')
+   if p is not None:
+      return f.generate_json_output(p,t)
+    
+   return '{"match":"none"}'
+
+if __name__ == '__main__':
+   app.run(debug=True)
