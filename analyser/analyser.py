4a18f9808d2eba92a154867e83ea5d5195bb555e
Barry Robinson <barry.w.robinson64@gmail.com>
2023-10-04 14:12:22 +0100

    make the analyser threaaded

--- analyser/analyser.py
+++ analyser/analyser.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 
-# Analysis module. This will be a mongodb app tha gathers arsed results from the
-# parsing app, and analyses them for preoblems. Remeber that in a real world app
-# the results would be from log files being collected of fcustomer networks. We
-# will simulate this and push new messages into our server. The server will tag
-# them with the customer ID and metadata about colection. All this, and the
-# results of parsing will then be stored in a mongdb database. Trigger sill be
-# set up so that when corrolations ocur, indicating a problem an alert will be
-# pushed to another app  that will be used to display status.
+# Analysis module. This will be a mongodb app tha gathers parsed results from
+# the parsing app, and analyses them for preoblems. Remeber that in a real world
+# app the results would be from log files being collected off customer networks.
+# We will simulate this and push new messages into our server. The server will
+# tag them with the customer ID and metadata about colection. All this, and the
+# results of parsing will then be stored in a mongdb database. Triggers ill be
+# set up so that when corrolations occur, indicating a problem, an alert will be
+# pushed to another app  that will be used to display status. This will probably
+# use some kind of pub / sub message queue, possibly rabbit.
 
 # We also want to do analysis on the parsed results as they come in to determin
 # the kind of event they represent. this needs to be abased on conditional logic
-# that asserts new values based on existinones. An example of this might be an
+# that asserts new values based on existing ones. An example of this might be an
 # integer value being resolved to 'ok','serious', and 'critical', which would be
-# asserted into metadata so that it can become part of the analysis.
+# asserted into metadata, or transformed in the message, so that it can become
+# part of the analysis.
 
 # Any language needs only act on JSON, so can use JQ syntax. 
 # .path == value 
@@ -35,37 +37,59 @@ from bson.json_util import loads
 import json
 from time import sleep
 from conversions import Converter
+import threading
+
 
 print("right at the start")
 
 # Setup the interface to our database
 client = MongoClient('mongodb://mongodb:27017')
-db = client['msg_db']['messages']
+msg_db = client['msg_db']['messages']
+update_db = client['update']['messages']
 cnv = Converter('rules')
 
 sleep(1)
 
 # TODO: Make this use native notfications instead of polling
 def process_new_messages(number):
+    msg_list = []
     print(f"processing new messages: {number}")
-    rec = db.find().sort('_id',-1).limit(number)
+    rec = msg_db.find().sort('_id',-1).limit(number)
     for r in rec:
         print(r)
-        msg = cnv.remap(r)
-        print(msg)
+        msg = cnv.reformat(r)
+        if msg is not None:
+            msg_list.append(msg)
+    return msg_list
 
 def run_analiser():
     print("starting analyser")
-    count = db.count_documents({})
+    count = msg_db.count_documents({})
     print(f"running analyser : {count}")
     while True:
-        tmp = db.count_documents({})
+        tmp = msg_db.count_documents({})
         if tmp > count:
-            process_new_messages(tmp - count)
+            msg_list = process_new_messages(tmp - count)
+
+            # Update the reformated db
+            for msg in msg_list:
+                print(msg)
+                msg.pop('_id',None)
+                Json = json.dumps(msg)
+                BJson = loads(Json)
+                update_db.insert_one(BJson)
+
             count = tmp
         sleep(1)
 
+def run_events():
+    while True:
+        pass
+
 if __name__ == '__main__':
     print("==> doing something...<==")
-    run_analiser()
+    athread = threading.Thread(target=run_analiser, daemon=True)
+    athread.start()
+    athread.join()
+    # run_analiser()
 
