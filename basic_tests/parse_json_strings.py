4d67b2d8bb1a9e24f8c1b5488de0f53504644a35
Barry Robinson <barry.w.robinson64@gmail.com>
2023-10-10 13:56:11 +0100

    refactor some of experiment

--- basic_tests/parse_json_strings.py
+++ basic_tests/parse_json_strings.py
@@ -1,9 +1,55 @@
 #!/usr/bin/env python3
 
 
-# import json
+import json
 import sys
 import re
+import pymongo
+
+class JSONQuery:
+    def __init__(self, table, query, constraint):
+        self._table = table
+        self._query = json.loads(query)
+        self._constraint = None
+        if constraint is not None:
+            self._constraint = json.loads(constraint)
+
+    def run_query(self, db):
+        try:
+            tbl = db.lookup_table(self._table)
+            if self._constraint is not None and self._query is not None:
+                return tbl.find(self._query,self._constraint)
+            elif self._query is not None:
+                return tbl.find(self._query)
+        except KeyError:
+            raise ValueError(f'unknown table {self._table}')
+
+
+class DistinctQuery:
+    def __init__(self, table, field):
+        self._field = field
+        self._table = table
+
+    def run_query(self,db):
+        tbl = db.lookup_table(self._table)
+        return [{self._field : tbl.distinct(self._field)}]
+
+
+class DB:
+    def __init__(self):
+        self._connection = pymongo.MongoClient('mongodb://localhost:27017')
+        self._lookup = {
+            'messages' : self._connection['msg_db']['messages'],
+            'updates'  : self._connection['msg_db']['updates'],
+            'status'   : self._connection['msg_db']['status']
+        }
+    
+    def lookup_table(self, tbl):
+        return self._lookup[tbl]
+    
+    def run_query(self,query):
+        return query.run_query(self)
+
 
 # Parse out strings and JSON content and split them into a list of found objects
 def isolate_json_or_string(st):
@@ -36,7 +82,7 @@ def isolate_json_or_string(st):
             word += c
             words.append(word)
             word = ''
-        elif c == '{' or c == '[':
+        elif (c == '{' or c == '['):
             if word != '':
                 words.append(word)
                 word = ''
@@ -61,21 +107,25 @@ def isolate_json_or_string(st):
     return words
 
 
-def main(st):
-    st = sys.argv[1]
+def parse_string(st):
     words = isolate_json_or_string(st)
+
+
+    # print(words)
     
     qname_regex = r'[a-z][a-z0-9\_]*'
-    exp_regex = r'\{\"[a-z\_]+\"\:.*\}'
+    exp_regex = r'\{\"[a-z\_\.\{\}]+\"\:.*\}'
+    # exp_regex = r'\{.*\}'
     collect_regex = r'messages|updates|status'
-
 
     collection = None
     query = None
     expression = None
     aggregate = False
     constraint = None
+    field = None
     expect = ''
+    pos = 0
     for word in words:
         if word == 'using':
             expect = 'collection'
@@ -92,9 +142,12 @@ def main(st):
                 print(f'unexpected token {word}')
                 break
             query = word
-            expect = 'expression'
-        elif expect == 'expression':
-            if re.match(exp_regex,word) is None:
+            expect = 'expression | sum'
+        elif expect == 'expression | sum':
+            if word == 'sum':
+                expect = 'field'
+                continue
+            elif re.match(exp_regex,word) is None:
                 print(f'unexpected token {word}')
                 break
             expression = word
@@ -106,6 +159,9 @@ def main(st):
             elif word == 'aggregate':    
                 aggregate = True
                 expect = 'end'
+            else:
+                print(f'unrecognised token {word}')
+                break
         elif expect == 'const_expre':
             constraint = word
             expect = 'aggregate | end'
@@ -115,23 +171,79 @@ def main(st):
                 if aggregate == False:
                     aggregate = True
                 else: 
-                    print('aggregate has already been set')
+                    print(f'aggregate has already been set: possition {pos}')
                     break
                 expect = 'end'
                 continue
             elif word == ';':
                 break
+        elif expect == 'field':
+            field = word
+            expect = 'end'
+            continue
         elif expect == 'end':
             if word != ';':
-                print('unexpected token')
+                print(f'unexpected token: {word}')
                 break
         else:
             print(f'unexpected token {word}')
             break
 
-                        
-    print(f'collection: "{collection}", query: "{query}", expression: "{expression}", constraint: "{constraint}", aggregate: "{aggregate}"')
+    if expression != None:
+        return (query,JSONQuery(collection,expression,constraint),aggregate)
+    else:
+        return (query,DistinctQuery(collection,field),False)
 
+
+
+# The goal of this function is to deduplicate entries and create a results map
+#   1. for all duplicate ewntries, create a metadata entry counting the number
+#      of duplicates in the form {'metadata' : {'field.value' : count}}
+#   2. where there is a new value for an existing field, cheange the entry to a
+#      list and add all entries, in the form {'field' : [value1,value2]}
+#   3. flatten the map so that all values can be counted in meta as jq paths as {'.name1.name2.name3' : count}
+# def aggregate
+def _aggregate_results(meta_label,label,map,results):
+    # value is result map so lable is guarenteed
+    val = map[label]
+
+    if isinstance(val,dict):
+        ml = meta_label + '.' + label
+        for l in val:
+            results = _aggregate_results(ml,l,val,results)
+        return results
+
+    meta = results['metadata']
+
+    # deal wit hhte metadata entry
+    lb = meta_label + '.' + label + '.' + str(val)
+    try:
+        count = meta[lb]
+        count +=1
+        meta[lb] = count 
+    except KeyError:
+        count = 1
+        meta[lb] = count
+
+    # now add back to the results
+    ml = meta_label + '.' + label
+    try:
+        val_res = results[ml]
+        if isinstance(val_res,list):
+            if val not in val_res:
+                val_res.append(val)
+        elif val != val_res:
+            results[ml] = [val_res,val]
+    except KeyError:
+        results[ml] = val
+
+    return results
+
+def aggregate_results(map,results):
+    for label in map:
+        results = _aggregate_results('',label,map,results)
+
+    return results
 
 # Get the value for a JSON query, this is the native format for a MongoDB query
 #  format 
@@ -140,5 +252,37 @@ def main(st):
 #     using <collection> <query name> <query> aggregate ;
 #     using <collection> <query name> <query> ; 
 
+def exec_statement(st,json_fmat):
+    (qname, q, aggregate) = parse_string(st)
+
+    db = DB()
+    l = []
+    res = q.run_query(db)
+
+    for r in res:
+        # remove becaus ethis will not serialise to JSON
+        r.pop("_id",None)
+        l.append(r)
+
+    if aggregate and len(l) > 0:
+        map = {'metadata':{}}
+        for r in l:
+            aggregate_results(r,map)    
+                
+        l = map
+
+    elif len(l) == 1:
+        l = l[0]
+
+    l = {qname:l}
+    
+    if json_fmat:
+        return json.dumps(l)
+        
+    return l
+
 if __name__ == "__main__":
-    main(sys.argv[1])
+    if sys.argv[1] == 'json':
+        print(exec_statement(sys.argv[2],True))
+    else:
+        print(exec_statement(sys.argv[2],False))
